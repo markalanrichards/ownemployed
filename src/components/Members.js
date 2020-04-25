cc6ce5f56f7aa48dc317540a25ef0d87b60e7f33
Mark Richards <mark.alan.richards@gmail.com>
2020-04-25 22:52:48 +0100

    Fixing styles

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Typography, Button } from 'antd'
 import PageLayout from './PageLayout'
 import UserCard from './UserCard'
 import users from '../data/users.json'
@@ -8,7 +9,9 @@ import users from '../data/users.json'
 const Members = () => (
   <PageLayout>
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', margin: 'auto' }}>
-      {Object.values(users).map((user) => (<UserCard user={user}/>))}
+      {Object.values(users).map((user) => (<UserCard user={user}
+                                                     style={{ height: '300px', width: '300px', margin: '16px', padding: '24px' }}
+      />))}
     </div>
   </PageLayout>
 )
